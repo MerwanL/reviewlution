e09fbd47e9bffe753b76a7ba95813bbb8567cd2f
Merwan L <merwan@insurlytech.com>
2020-11-26 20:31:53 +0100

    refacto and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { OriginComponent } from "./origin/origin.component";
+import { ProductComponent } from "./product/product.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+    pathMatch: "full",
+  },
+  {
+    path: "origine",
+    component: OriginComponent,
+    pathMatch: "full",
+  },
+  {
+    path: "produits",
+    component: ProductComponent,
+    pathMatch: "full",
+  },
+  { path: "**", redirectTo: "" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
